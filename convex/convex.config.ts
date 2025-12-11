31ce9a84307d251e60ecde8006c873fd1ea3f5ee
Maxime-NS <maxime.gey@nativesquare.fr>
2025-12-11 14:54:02 +0100

    fix: lucide react native icons setup

--- convex/convex.config.ts
+++ convex/convex.config.ts
@@ -1,9 +1,7 @@
 import migrations from "@convex-dev/migrations/convex.config";
-import resend from "@convex-dev/resend/convex.config";
 import { defineApp } from "convex/server";
 
 const app = defineApp();
 app.use(migrations);
-app.use(resend);
 
 export default app;
